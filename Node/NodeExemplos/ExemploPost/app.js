d6171491cefbc4c54a2b44be10cd1b3e5e241623
nicolaskmb <nicolasdark163@gmail.com>
2022-09-28 19:26:26 -0300

    committed

---
+++ Node/NodeExemplos/ExemploPost/app.js
@@ -0,0 +1,37 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+
+let router = express.Router();
+let app = express();
+
+app.use(express.static(__dirname));
+app.use("/",router);
+
+let encodeUrl = bodyParser.urlencoded({ extended: false });
+
+router.get('/', function(req,res){
+    res.sendfile("login.html");
+});
+
+router.post('/', encodeUrl, (req, res) => {
+    console.log("Post da aplicacão");
+
+    console.log("Usuario: " + req.body.usuario);
+    console.log("senha: " + req.body.senha);
+    let usuario = req.body.usuario;
+    let senha = req.body.senha;
+
+    if (usuario == "nicolas" && senha == "123") {
+      res.end("Acesso liberado!!!!!");  
+    } else {
+        res.end("<script>alert('Usuário ou senha incorretos!'); history.go(-1); </script>");  
+    }
+
+    
+} );
+
+
+app.listen(3000, () => {
+    console.log("Aplicação de login com post subiu na porta 3000")
+})
+
